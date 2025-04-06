9e0e1108c7630c3c681d113baaf9fff817f519c5
matth <matthewiva13@gmail.com>
2025-04-06 13:35:46 +0300

    faster hash map

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
 use bpers;
 
 fn main() {
-    let input = std::fs::read_to_string("src/main.rs").unwrap();
-    // let input = "aa aa aa aa aa aa";
+    let input = std::fs::read_to_string("bible.txt").unwrap();
+    // let input = "aa ab ab aabb aab bb aab aab aa aa";
     let mut vocabulary = bpers::Vocabulary::new();
-    let tokenized = vocabulary.learn(&input, 99999);
+    let tokenized = vocabulary.learn(&input, 2000);
     println!(
         "input len: {}; tokenized len: {}",
         input.len(),
         tokenized.len()
     );
 
-    println!("{:#?}", vocabulary.id_to_token);
-
     let encoded = bpers::encode(&input, &vocabulary);
-    println!("{:?}", tokenized);
-    println!("{:?}", encoded);
     println!("encoded size: {}", encoded.len());
 
-    let decoded = bpers::decode(&encoded, &vocabulary);
-    println!("{decoded}");
+    let decoded = bpers::decode(&tokenized, &vocabulary);
+    // println!("{decoded}");
     println!("decoded size: {}", decoded.len());
 }
